05ebef37dc2aaa6ae0be39d79a9e1ac7e5894ea5
viswa1220 <viswanathan122000@gmail.com>
2025-02-17 01:30:55 -0500

    routes learning

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
  const express=require("express")
 
  const app=express();
+ /* get : will match only /user  */
+
+ app.get("/user",(req,res)=>{
+   res.send("user get");
+ });
+ app.post("/user",(req,res)=>{
+   res.send("data saved");
+ });
+
+ /* Use : will match all the http method api calls  */
 
  ///req handler
  app.use("/test",(req,res)=>{
     res.send("hello from server!!!");
  })
+ app.use("/",(req,res)=>{
+   res.send("hello Stranger!!!");
+})
  //listen port
  app.listen(3000, () => {
     console.log("running in 3000");
