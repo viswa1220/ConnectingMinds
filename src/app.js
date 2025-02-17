1641f1df7a7cdb33010742736fd6adfbc1732eed
viswa1220 <viswanathan122000@gmail.com>
2025-02-17 04:22:42 -0500

    learned to create user schema and try to hit the api and post and save in db

--- src/app.js
+++ src/app.js
@@ -1,48 +1,37 @@
 const express = require("express");
 const { adminAuth } = require("./middlewares/auth");
+const connectDb = require("./config/database");
 const app = express();
+const User = require("./models/user");
 /* get : will match only /user  */
-
-app.get("/user", (req, res) => {
-  res.send("user get");
-});
-app.post("/user", (req, res) => {
-  res.send("data saved");
-});
-
-/* Use : will match all the http method api calls  */
-
-///req handler
-app.use("/test", (req, res) => {
-  res.send("hello from server!!!");
-});
-
-/* handle auth for all req get post patch delete put*/
-app.use("/admin", adminAuth);
-
-app.use("/admin/test", (req, res) => {
-  res.send("hello from test server!!!");
-});
-app.use("/admin/testone", (req, res) => {
-  res.send("hello from test 1 server!!!");
-});
-
-app.use("/userwithoutAuth", (req, res) => {
-  res.send("userwithoutAuth");
-});
-
-// if there is no response it will hang and next will be used to go for second response;
-app.use(
-  "/stranger",
-  (req, res, next) => {
-    // res.send("hello Stranger!!!");
-    next();
-  },
-  (req, res) => {
-    res.send("hello Stranger!!! 2");
+app.post("/signup", async (req, res) => {
+  const userObject = {
+    firstName: "Ms",
+    lastName: "Dhoni",
+    emailId: "Ms@gmail.com",
+    password: "Viswa@123",
+    age: 23,
+    gender: "Male",
+    experience: 2,
+  };
+  //creating a new instance of user model
+  const user = new User(userObject);
+  try {
+    await user.save();
+    res.send("user added");
+  } catch (err) {
+    res.status(401).send("Error in User Adding" + err.message);
   }
-);
-//listen port
-app.listen(3000, () => {
-  console.log("running in 3000");
 });
+
+connectDb()
+  .then(() => {
+    console.log("Connected to mongo Db");
+    //listen port
+    app.listen(3000, () => {
+      console.log("running in 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("error to connect mongo Db");
+  });
